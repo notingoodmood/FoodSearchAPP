076eb8e4657760130f6a5ed3fa947431ff809a2a
lvzhiwei02 <lvzhiwei02@meituan.com>
2021-04-04 19:22:46 +0800

    初始化

---
+++ FoodSearch/info.js
@@ -0,0 +1,15 @@
+//存放运行时关键信息与方法
+
+//服务器IP
+export const server_ip='1.13.4.44';
+
+//服务器端口
+export const server_port='8080';
+
+//本地缓存：历史记录 List
+export const HISTORY_LIST='history_list';
+
+//历史记录条目数据Object
+export interface{
+	
+}
