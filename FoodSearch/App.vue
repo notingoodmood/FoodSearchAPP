aef17c2b53ef60a942cfec4eedc29e07c7173a81
lvzhiwei02 <zhiweilv@sohu-inc.com>
2021-05-13 20:33:58 +0800

    假登录界面

--- FoodSearch/App.vue
+++ FoodSearch/App.vue
@@ -2,6 +2,13 @@
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			uni.navigateTo({
+				url:"pages/user_login/user_login"
+			})
+			uni.showToast({
+				title:"请登录...",
+				icon:"loading"
+			})
 		},
 		onShow: function() {
 			console.log('App Show')
